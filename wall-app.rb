6d3f2ed882ba86114d0544a4ba16a3e0e5527e36
dantetam <dante.tam1@gmail.com>
2014-09-29 17:18:46 +0000

    Adding delete button

--- wall-app.rb
+++ wall-app.rb
@@ -11,17 +11,24 @@ class Message
   property :created_at, DateTime, required: true
   property :likes,      Integer,  required: true
   property :dislikes,   Integer,  required: true
+  property :shown,      Integer,  required: true
   
-  def addLike()
-    self.likes += 1
+  def addLike() 
+    self.likes += 1 
   end
-  def subLike()
-    self.likes -= 1
+  def subLike() 
+    self.dislikes += 1 
   end
+  def hide()
+    self.shown = 0
+  end
+  
 end
 
 DataMapper.finalize()
 DataMapper.auto_upgrade!()
+
+
 
 get("/") do
   records = Message.all(order: :created_at.desc)
@@ -37,17 +44,38 @@ get ("/submitOpinion") do
     message = records[params["opinion"][4..params["opinion"].length].to_i]
     message.subLike()
   elsif params["opinion"][0..3] == "Impl" 
-  
+    message = records[params["opinion"][4..params["opinion"].length].to_i]
+    message.hide()
   end
   message.save
   redirect("/")
 end
 
+=begin
+get ("/like") do
+  records = Message.all(order: :created_at.desc)
+  message = records[params["opinion"].to_i]
+  message.addLike()
+  message.save
+end
+
+get ("/hate") do
+  records = Message.all(order: :created_at.desc)
+  message = records[params["opinion"].to_i]
+  message.subLike()
+  message.save
+end
+
+get ("/implode") do
+  redirect("/")
+end
+=end
+
 post("/messages") do
 
   message_body = params["body"]
   
-  message = Message.create(body: params["body"], created_at: DateTime.now, likes: 0, dislikes: 0)
+  message = Message.create(body: params["body"], created_at: DateTime.now, likes: 0, dislikes: 0, shown: 1)
 
   if message.saved?
     redirect("/")
